131c005dac8818e06124d050c098d73c40065530
P Luong <p-luong@live.co.uk>
2022-07-21 16:33:25 +0100

    updated questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -2,6 +2,80 @@
 
 var questions = [
   (
-    
-  )
+    numb: 1,
+    questions: "1. Javascript is an _______ language?",
+    answer: "Object-Oriented"
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  ),
+  (
+    numb: 2,
+    questions: "2. Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B"
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  ),
+  (
+    numb: 3,
+    questions: "3. Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B"
+    options: [
+      "getElementbyId()",
+      "getElementsByClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  ),
+  (
+    numb: 4,
+    questions: "4. Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answer: "Ignores the statements"
+    options: [
+      "Throws an Error",
+      "Ignores the statements",
+      "Gives a warning",
+      "None of the above"
+    ]
+  ),
+  (
+    numb: 5,
+    questions: "5. Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above"
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above"
+    ]
+  ),
+  (
+    numb: 6,
+    questions: "6. How can a datatype be declared to be a constant type?",
+    answer: "const"
+    options: [
+      "const",
+      "var",
+      "let",
+      "constant"
+    ]
+  ),
+  (
+    numb: 7,
+    questions: "7. Which of the following is not a Javascript framework?",
+    answer: "Cassandra"
+    options: [
+      "Node",
+      "Vue",
+      "React",
+      "Cassandra"
+    ]
+  ),
 ]
