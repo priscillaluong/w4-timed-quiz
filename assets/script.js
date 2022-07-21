a44a2ffe17c56418df825b6d4d6c3a50794ecedf
P Luong <p-luong@live.co.uk>
2022-07-21 15:50:02 +0100

    updated javscript, added global variables and start button event listener

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
 // Global variables
 
-var startBtn = document.querySelector("#start");
-var timerEl = document.getElementById('countdown');
+var timerCountdown = document.querySelector("#timer");
+
+var startContainer = document.querySelector(".start-container"); 
+var startBtn = document.querySelector(".start-button #start");
+var quizContainer = document.querySelector(".quiz-container"); 
+
+//if Start Button Clicked, the quiz and countdown is started
+
+startBtn.addEventListener("click", function() {
+    //countdown(); //run countdown function
+    startContainer.setAttribute("style",  "display: none");
+    quizContainer.setAttribute("style",  "display: flex");
+});
+
+var quizContainer = document.querySelector(".quiz-container");  
+//var timerEl = document.getElementById('countdown');
 
 // Functions
 
-function countdown() {
+function countdown() { 
   var timeLeft = 5;
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
@@ -17,12 +31,6 @@ function countdown() {
       displayMessage();
     }
   }, 1000);
-}
-
-// After start quiz button is clicked the quiz and countdown starts
-function startQuiz() {
-    countdown();
-
 }
 
 // Displays the message
@@ -37,12 +45,12 @@ startBtn.addEventListener("click", countdown);
 
 // HIGHSCORES - can we refer to the increment decrement activity to code this?
 
-if ()
+//if ()
 
 
 
 // we may need to use this to carry out another function after a form has been submitted. E.g. print amount
-event.preventDefault();
+//event.preventDefault();
 
 // put each question into an array so we can go through the q's with an index
 
